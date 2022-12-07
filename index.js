7778c9bc1701bc622dc3b1f54da76e6349c37764
Harry Torres <Harrywt24@gmail.com>
2022-12-06 21:34:30 -0500

    commit tree ffb629bd648929f02f206b6468d92219f801570c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+const event = "birthday";
+const messages = [];
+function writeCards(names, event){
+    for (let i = 0; i < names.length; i++){
+       messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    console.log(messages);
+    return messages;
+    }
+
+function countDown() {let i = 10;
+while (i >= 0) {
+    console.log(i--);
+}}
